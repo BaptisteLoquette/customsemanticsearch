f58a93402cd6dfa34f95b13f969d1a5373c69505
Bap t <bapt@MacBook-Air-de-Bap.local>
2023-08-11 11:42:36 +0200

    Adding .py files for MMARCO dataset preprocessing for query generation

---
+++ src/preprocessing/preprocess_mmarco.py
@@ -0,0 +1,49 @@
+"""
+Preprocess mmarco dataset for Query Generation
+"""
+
+from transformers import T5Tokenizer
+from datasets import load_from_disk
+import argparse
+import os
+
+from src.utils.preprocessing_utils import clean_and_format, tokenize_dset_query_gen
+
+parser  =   argparse.ArgumentParser(
+    prog="preprocess_mmarco",
+    description="Preprocess unicamp-dl/mmarco dataset for Query Generation"
+)
+parser.add_argument('out', help="Output dir of the preprocessed files", type=str)
+parser.add_argument('--n_training_samples', help="Number of training samples to train the model on. For everything type 'all'", type=int, default=500_000)
+parser.add_argument('--dset_path', help="Path of download mmarco dataset", type=str, default="data/mmarco_queries_dset")
+args    =   parser.parse_args()
+
+if "__main__" == __name__:
+    output_dir      =   args.out
+    dset_path       =   args.dset_path
+    dset            =   load_from_disk(dset_path)
+    n_samples       =   len(dset) if args.n_training_samples=='all' else int(args.n_training_samples)
+
+    # Select the train & valid subsets
+    dset    =   dset.select([i for i in range(n_samples)])
+    dset    =   dset.map(clean_and_format)
+
+    # Separate the dataset into a positive examples dataset and negative example datasets
+    # This allows for training t5 to generate positive queries, and/or to generate hard negatives queries to ameliorate the retriever's accuracy
+    negative_dset   =   dset.remove_columns("positive").rename_column("negative", "document")
+    positive_dset   =   dset.remove_columns("negative").rename_column("positive", "document")
+
+    # Tokenize the datasets
+    tokenizer       =   T5Tokenizer.from_pretrained('t5-small')
+    positive_dset   =   positive_dset.map(tokenize_dset_query_gen, fn_kwargs={"tokenizer": tokenizer})
+    positive_dset   =   positive_dset.remove_columns(["query", "document"])
+    negative_dset   =   negative_dset.map(tokenize_dset_query_gen, fn_kwargs={"tokenizer": tokenizer})
+    negative_dset   =   negative_dset.remove_columns(["query", "document"])   # Remove str columns
+
+    # Shuffle datasets
+    positive_dset   =   positive_dset.shuffle()
+    negative_dset   =   negative_dset.shuffle()
+
+    # Save Datasets
+    positive_dset.save_to_disk(os.path.join(output_dir, "positive"))
+    negative_dset.save_to_disk(os.path.join(output_dir, "negative"))
---
+++ src/utils/preprocessing_utils.py
@@ -0,0 +1,34 @@
+"""
+Utility functions for Preprocessing
+"""
+
+import copy
+from typing import Dict
+from transformers import T5Tokenizer
+
+def clean_and_format(sample:Dict[str]):
+    """Convert each column to ASCII and modify the string by adding a prefix to each column"""
+    sample['query']     =   f"{sample['query'].encode('ascii', 'ignore').decode('ascii')}"
+    sample['positive']  =   f"generate_positive_query: {sample['positive'].encode('ascii', 'ignore').decode('ascii')}"
+    sample['negative']  =   f"generate_negative_query: {sample['negative'].encode('ascii', 'ignore').decode('ascii')}"
+    
+    return sample
+
+def tokenize_dset_query_gen(sample:Dict[str], tokenizer:T5Tokenizer):
+    """Tokenizes Dataset using T5-small tokenizer (SentencePiece)"""
+    source  =   tokenizer.encode_plus(sample['document'], max_length=512,
+                                    truncation=True, padding='max_length',
+                                    return_tensors="pt")                # Truncating over 512
+    target  =   tokenizer.encode_plus(sample['query'], max_length=96,
+                                      truncation=True, padding='max_length',
+                                      return_tensors="pt")
+    
+    sample['source_ids']            =   source['input_ids']
+    sample['source_attention_mask'] =   source['attention_mask']
+    sample['target_ids']            =   target['input_ids']
+    sample['target_attention_mask'] =   target['attention_mask']
+    labels  =   copy.deepcopy(target['input_ids'])
+    labels[labels==0]   =   -100    # Replace <pad>'s idx (0) with -100 in order for t5 to not compute the loss on the paddings
+    sample['labels']    =   labels
+
+    return sample
